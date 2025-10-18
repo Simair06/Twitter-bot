b14ab48ecf19620599ce4fe621a29c1e7bee9777
Simair06 <simi.mair06@gmail.com>
2025-10-18 12:12:02 +0200

    git commit

--- Livebot.py
+++ Livebot.py
@@ -9,7 +9,6 @@ import schedule
 
 current_matches_url = "https://api.openligadb.de/getmatchdata/bl1"
 matchday_url = "https://api.openligadb.de/getcurrentgroup/bl1"
-uhrzeit = "11:46"
 
 print(datetime.datetime.now())
 
@@ -21,26 +20,27 @@ def api(url):
 
 
 def get_matchday():
-    global matchday
     matchday = api(matchday_url)
     matchday = matchday["groupName"]
-    print(matchday)
     return matchday
     
 
 def get_matches():
-    global this_week
     this_week = api(current_matches_url)
-    print(this_week)
     return this_week
 
 def main():
-    schedule.every().saturday.at(uhrzeit).do(get_matchday)
-    schedule.every().saturday.at(uhrzeit).do(get_matches)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    matchday = get_matchday()
+    this_week = get_matches()
+    print(matchday)
+    print(this_week)
+
+    
 
 
-main()
+schedule.every().saturday.at("12:11:50").do(main)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
 
