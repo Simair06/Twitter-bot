34f3546ad77f14a0ccd76be58748db595afd6063
Simair06 <simi.mair06@gmail.com>
2025-10-18 11:54:23 +0200

    git commit

--- Livebot.py
+++ Livebot.py
@@ -4,19 +4,43 @@ import threading
 import requests
 import os
 from dotenv import load_dotenv
+import schedule
+#https://www.openligadb.de/
 
-def api():
-    load_dotenv()
-    api_key = os.getenv("API2_KEY")
-    url = "https://v3.football.api-sports.io/leagues?id=78"
-    headers = {
-        "x-apisports-key": api_key
-    }
+current_matches_url = "https://api.openligadb.de/getmatchdata/bl1"
+matchday_url = "https://api.openligadb.de/getcurrentgroup/bl1"
+uhrzeit = "11:46"
 
-    print(api_key)
+print(datetime.datetime.now())
 
-    response = requests.get(url, headers=headers)
+
+def api(url):
+    response = requests.get(url)
     data = response.json()
-    print(data)
+    return data
 
-api()
+
+def get_matchday():
+    global matchday
+    matchday = api(matchday_url)
+    matchday = matchday["groupName"]
+    print(matchday)
+    return matchday
+    
+
+def get_matches():
+    global this_week
+    this_week = api(current_matches_url)
+    print(this_week)
+    return this_week
+
+def main():
+    schedule.every().saturday.at(uhrzeit).do(get_matchday)
+    schedule.every().saturday.at(uhrzeit).do(get_matches)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+main()
+
